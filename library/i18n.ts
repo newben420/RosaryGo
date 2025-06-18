cc1886ff7b9a50dad207d08399dfaf9606c7f69c
newben420 <wraith@wraith.wraith>
2025-06-18 11:13:40 +0100

    added 3 more languages

--- library/i18n.ts
+++ library/i18n.ts
@@ -6,6 +6,9 @@ import la from './../locales/la.json';
 import es from './../locales/es.json';
 import fr from './../locales/fr.json';
 import pt from './../locales/pt.json';
+import it from './../locales/it.json';
+import tl from './../locales/tl.json';
+import sw from './../locales/sw.json';
 
 export const locales = [
     {code: 'en', label: 'English'},
@@ -13,10 +16,13 @@ export const locales = [
     {code: 'es', label: 'Español'},
     {code: 'fr', label: 'Français'},
     {code: 'pt', label: 'Português'},
+    {code: 'it', label: 'Italiano'},
+    {code: 'tl', label: 'Tagalog'},
+    {code: 'sw', label: 'Kiswahili'},
 ]
 
-const i18 = new I18n({
-    en, la, es, fr, pt,
+const i18 = new I18n({ 
+    en, la, es, fr, pt, it, tl, sw,
 });
 
 const deviceLanguage = getLocales()[0].languageCode || locales[0].code;
